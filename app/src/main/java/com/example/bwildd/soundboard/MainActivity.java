e898a4469633aca7b13ece321a7900f0b311d370
Unknown <weibelmikko@gmail.com>
2019-06-05 09:13:10 +0200

    Audio recording and permission added

--- app/src/main/java/com/example/bwildd/soundboard/MainActivity.java
+++ app/src/main/java/com/example/bwildd/soundboard/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.bwildd.soundboard;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton mAddNewAudio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAddNewAudio = (FloatingActionButton) findViewById(R.id.addActionBtn);
+
+        mAddNewAudio.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), crudActivity.class);
+                //intent.putExtra("badiId", selected.getId());
+                //intent.putExtra("badiName", selected.
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
