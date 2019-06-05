822c3843d7e1563f23783864b8f45bb4db796f99
David Wildi <david.wildi@gmx.ch>
2019-06-05 09:11:31 +0200

    Add to DB

--- app/src/main/java/com/example/bwildd/soundboard/MainActivity.java
+++ app/src/main/java/com/example/bwildd/soundboard/MainActivity.java
@@ -2,12 +2,56 @@ package com.example.bwildd.soundboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.bwildd.soundboard.db.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseHelper mDatabaseHelper;
+    private Button btnAdd;
+    private EditText txtAdd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mDatabaseHelper = new DatabaseHelper(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtAdd = findViewById(R.id.txtTitel);
+        btnAdd = findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newEntry = txtAdd.getText().toString();
+
+                if (newEntry.length() != 0) {
+                    AddData(newEntry);
+                    txtAdd.setText("");
+                } else {
+                    toastMessage("put something in the field");
+                }
+            }
+        });
+    }
+
+    public void AddData(String name, String path, int favorite) {
+        Boolean insertData;
+        insertData = mDatabaseHelper.addData(name, path, favorite);
+
+        if (insertData) {
+            toastMessage("Data Inserted");
+        } else {
+            toastMessage("Something went wrong");
+        }
+    }
+
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/bwildd/soundboard/db/DatabaseHelper.java
@@ -0,0 +1,52 @@
+package com.example.bwildd.soundboard.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = "DatabaseHelper";
+
+    private static final String DATABASE_NAME = "Soundboard.db";
+    private static final String TABLE_NAME = "soundboard";
+    private static final String COL1 = "id";
+    private static final String COL2 = "name";
+    private static final String COL3 = "file";
+    private static final String COL4 = "favorite";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE soundboard (" + COL1 + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL2 + " TEXT, " + COL3 + " TEXT, " + COL4 + " INTEGER);";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String dropIfExists = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
+        db.execSQL(dropIfExists);
+        onCreate(db);
+    }
+
+    public boolean addData(String name, String path, int favorite) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2, name);
+        contentValues.put(COL3, path);
+        contentValues.put(COL4, favorite);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
